3792d0b997e38a75df0c285896a616c7c2831e3a
AbdelRahman Nasser <anasser@arpuplus.com>
2023-01-23 15:50:15 +0200

    3 different ways to execute javascript code

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHelloFromHead() {
+    alert("Hello world from head tag.")
+}
+
+function sayHelloFromBody() {
+    alert("Hello world from body tag.")
+}
+
+
+function sayHelloFromEventDriven() {
+    alert("Hello world from body tag.")
+}
