54c84fff75db291433af421984dc892d221b062c
AbdelRahman Nasser <anasser@arpuplus.com>
2023-01-23 17:41:46 +0200

    Commit more practice built in func in Javascript

--- script.js
+++ script.js
@@ -4,9 +4,28 @@ function sayHelloFromHead() {
 
 function sayHelloFromBody() {
     alert("Hello world from body tag.")
+    console.log(Number("33a"));
+    console.log(parseInt("33a"));
+    console.log(parseInt("a33"));
+
+
 }
 
 
 function sayHelloFromEventDriven() {
     alert("Hello world from body tag.")
+}
+
+function TestPromptAndConfirm(){
+    var name = prompt("Enter your name: ","");
+
+    alert("Welcome "+ name);
+
+    var result = confirm("Are you sure?");
+    if(result == true){
+        alert("Thanks...");
+    }
+    else{
+        alert("GoodBye...");
+    }
 }
