3af23fa0a705f1fface15c8b773b38440032212a
AbdelRahman Nasser <anasser@arpuplus.com>
2023-02-15 16:27:21 +0200

    Learn data types of variables by typescript

--- script.js
+++ script.js
@@ -10,17 +10,43 @@ var circu = function (diameter) {
 };
 console.log(circu(5));
 /*Arrays*/
-var strings = ['khaled', 'reda', 'monir', 'tarek']; // strings only
+var strings = ["khaled", "reda", "monir", "tarek"]; // strings only
 strings[4] = "tamer";
 for (var i = 0; i < strings.length; i++) {
-    console.log('number index' + i + ': ' + strings[i]);
+    console.log("number index" + i + ": " + strings[i]);
 }
 var numbers = [1, 2, 3, 4];
 for (var i = 0; i < numbers.length; i++) {
-    console.log('number index' + i + ': ' + numbers[i]);
+    console.log("number index" + i + ": " + numbers[i]);
 }
-var mixedArray = ['tamer', 3, 4, 'sa'];
+var mixedArray = ["tamer", 3, 4, "sa"];
 mixedArray[0] = 1;
 for (var i = 0; i < mixedArray.length; i++) {
-    console.log('number index' + i + ': ' + mixedArray[i]);
+    console.log("number index" + i + ": " + mixedArray[i]);
 }
+var ninja;
+ninja = {
+    name: "tarek",
+    belt: "red",
+    age: 24
+};
+var ninjaTwo;
+ninjaTwo = {
+    name: "tarek",
+    age: 24,
+    beltColor: "red"
+};
+var numbs = []; // initialize it to avoid error
+numbs.push(1);
+//union types
+var mixedValues = [];
+mixedValues.push("hello");
+mixedValues.push(0);
+mixedValues.push(true);
+console.log(mixedValues);
+var uid;
+uid = "123";
+uid = 123;
+//dynamic assign (any)
+var dynamicValue;
+var dynamicArray = [];
--- script.ts
+++ script.ts
@@ -1,32 +1,70 @@
-function myFunction() : void {
-    let firstName: string = "Abdelrahman";
-    let secondName = "Nasser";
-    console.log(typeof firstName +" "+typeof secondName);
+function myFunction(): void {
+  let firstName: string = "Abdelrahman";
+  let secondName = "Nasser";
+  console.log(typeof firstName + " " + typeof secondName);
 
-    const json = JSON.parse("55");
-    console.log(typeof json);
+  const json = JSON.parse("55");
+  console.log(typeof json);
 }
 
 const circu = (diameter: number) => {
-    return diameter * Math.PI;
-}
+  return diameter * Math.PI;
+};
 
 console.log(circu(5));
 
 /*Arrays*/
 
-let strings = ['khaled','reda','monir','tarek'] // strings only
-strings[4] = "tamer"
-for (let i = 0 ; i < strings.length ; i++){
-    console.log('number index'+ i + ': '+ strings[i])
+let strings = ["khaled", "reda", "monir", "tarek"]; // strings only
+strings[4] = "tamer";
+for (let i = 0; i < strings.length; i++) {
+  console.log("number index" + i + ": " + strings[i]);
 }
 
-let numbers = [1,2,3,4]
-for (let i = 0 ; i < numbers.length ; i++){
-    console.log('number index'+ i + ': '+ numbers[i])
+let numbers = [1, 2, 3, 4];
+for (let i = 0; i < numbers.length; i++) {
+  console.log("number index" + i + ": " + numbers[i]);
 }
-let mixedArray = ['tamer',3,4,'sa']
-mixedArray[0]=1
-for (let i = 0 ; i < mixedArray.length ; i++){
-    console.log('number index'+ i + ': '+ mixedArray[i])
+let mixedArray = ["tamer", 3, 4, "sa"];
+mixedArray[0] = 1;
+for (let i = 0; i < mixedArray.length; i++) {
+  console.log("number index" + i + ": " + mixedArray[i]);
 }
+
+let ninja: object;
+
+ninja = {
+  name: "tarek",
+  belt: "red",
+  age: 24,
+};
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColor: string;
+};
+ninjaTwo = {
+  name: "tarek",
+  age: 24,
+  beltColor: "red",
+};
+let numbs: number[] = []; // initialize it to avoid error
+numbs.push(1);
+
+//union types
+let mixedValues: (string | boolean | number)[] = [];
+mixedValues.push("hello");
+mixedValues.push(0);
+mixedValues.push(true);
+console.log(mixedValues);
+
+let uid: string | number;
+uid = "123";
+uid = 123;
+
+
+
+//dynamic assign (any)
+let dynamicValue : any;
+let dynamicArray : any[] = [];
